9d96ffac26a1f3983f14fbe26b017bc91402dc01
cristianscaliaarrow <30871073+cristianscaliaarrow@users.noreply.github.com>
2017-09-18 11:21:02 -0300

    FirstCommit

---
+++ Assets/OneSignalManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneSignalManager : MonoBehaviour {
+
+   // public GUIStyle style;
+    public ReadMessages readMessage;
+
+    void Start()
+    {
+        OneSignal.StartInit("840b0c45-e9cc-47d7-8b40-94f703931e50").HandleNotificationOpened(HandleNotificationOpened).HandleNotificationReceived(HandleNotificationReceived).EndInit();
+    }
+
+   
+    private void HandleNotificationOpened(OSNotificationOpenedResult result)
+    {
+        readMessage.LoadMessagesFromFile();
+        Message m = new Message();
+        m.title = result.notification.payload.title;
+        m.message = result.notification.payload.body;
+        readMessage.pendingMessages.Add(m);
+        readMessage.UpdatePopUpPendingMessages();
+        readMessage.SaveAllMessage();
+        Debug.Log("POKE " + m.title + " " + m.message);
+    }
+
+
+    private void HandleNotificationReceived(OSNotification notification)
+    {
+        OSNotificationPayload payload = notification.payload;
+        string message = payload.body;
+    }
+
+   /* private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "ASDD",style);
+    }*/
+}
---
+++ Assets/Unimgpicker/Samples/PickerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Kakera
+{
+    public class PickerController : MonoBehaviour
+    {
+        [SerializeField]
+        private Unimgpicker imagePicker;
+
+        [SerializeField]
+        private Image imageRenderer;
+
+        void Awake()
+        {
+            imagePicker.Completed += (string path) =>
+            {
+                StartCoroutine(LoadImage(path, imageRenderer));
+            };
+        }
+
+        public void Start()
+        {
+           // OnPressShowPicker();
+        }
+
+        public void OnPressShowPicker()
+        {
+            imagePicker.Show("Select Image", "unimgpicker", 1024);
+        }
+
+        private IEnumerator LoadImage(string path, Image output)
+        {
+
+            var url = "file://" + path;
+            var www = new WWW(url);
+            yield return www;
+            yield return new WaitForSeconds(0.5f);
+            var texture = www.texture;
+            if (texture == null)
+            {
+                Debug.LogError("Failed to load texture url:" + url);
+            }else
+            {
+                PlayerPrefs.SetString("imagePath-" + Login.debugUser.id, path);
+            }
+            output.transform.eulerAngles = new Vector3(0, 0, -90);
+            output.sprite = Sprite.Create((Texture2D)texture, new Rect(0, 0, texture.width, texture.height), Vector3.one / 2);
+            Login.UpdateGUI();
+        }
+    }
+}
